c523e76db0b51467fbf0ed987ac8f8b9703beb32
AFellowCoderUser <109687165+AFellowCoderUser@users.noreply.github.com>
2023-10-28 17:45:20 -0400

    First commits to website

--- public/javascript/footer.js
+++ public/javascript/footer.js
@@ -1,6 +1,9 @@
 var footer = '<p>Created: 2023/06/08</p>'
 
+footer += '<div style="font-size:small">'
 footer += '<p>© DigitialMebi 2023</p>'
+footer += '<p>Powered by Neocities and Github</p>'
+footer += '</div>'
 
 footer += '<p><a href="#">To the top, please.</a></p>'
 
