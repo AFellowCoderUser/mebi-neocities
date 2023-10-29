0fd0c82f2d548a2c8e3a5751de3aafb6074e3da7
AFellowCoderUser <109687165+AFellowCoderUser@users.noreply.github.com>
2023-10-29 14:24:57 -0400

    Attempt #15: a

--- public/javascript/styleswitcher.js
+++ public/javascript/styleswitcher.js
@@ -7,12 +7,13 @@ function waitLoad(){
 }
 
 function imagechange(val){
-  if (val == "mebi")
+  if (val == "mebi") {
      document.getElementById("cssbanner").src = "/images/page_deco/banner.png"
      document.getElementById("cssnix_kate").src = "/images/page_deco/nix_kate.png";
-  if (val == "halloween")
-    document.getElementById("cssbanner").src = "/images/page_deco/halloween/banner.png"
-    document.getElementById("cssnix_kate").src = "/images/page_deco/halloween/nix_kate.png";
+  } else if (val == "halloween") {
+  document.getElementById("cssbanner").src = "/images/page_deco/halloween/banner.png"
+  document.getElementById("cssnix_kate").src = "/images/page_deco/halloween/nix_kate.png";
+  }
 }
 
 function changeStyle(style, js) {
