fcd1bb290c8d54c91484b5523b6ca558f225dd47
AFellowCoderUser <109687165+AFellowCoderUser@users.noreply.github.com>
2023-11-01 13:53:06 -0400

    wiki update, sitemap, thumbnail

--- public/javascript/footer.js
+++ public/javascript/footer.js
@@ -1,7 +1,7 @@
 var footer = '<p>Created: 2023/06/08</p>'
 
 footer += '<div style="font-size:small">'
-footer += '<p>© DigitialMebi 2023</p>'
+footer += '<p>© DigitialMebi 2023 -- <a href="/sitemap">Sitemap</a></p>'
 footer += '<p>Powered by <a href="https://neocities.org/site/afellowu">Neocities</a> and <a href="https://github.com/AFellowCoderUser/mebi-neocities">Github</a></p>'
 footer += '</div>'
 
--- public/javascript/topbarnav.js
+++ public/javascript/topbarnav.js
@@ -1,17 +1,17 @@
 //credit later: https://templaterr.neocities.org/topbar/
 
-var html = '<a href="https://afellowu.neocities.org/">Home</a>';
+var html = '<a href="/index">Home</a>';
 
-html += '<a href="https://afellowu.neocities.org/about">About Me</a>'
+html += '<a href="/about">About Me</a>'
 
 html += '<div id="dropdown">'
 html += '<button id="dropbtn">Links -</button>'
 html += '<div id="dropdown-content">'
-html += '<a href="https://afellowu.neocities.org/socials">Socials</a>'
-html += '<a href="https://afellowu.neocities.org/sites">Sites</a>'
+html += '<a href="/socials">Socials</a>'
+html += '<a href="/sites">Sites</a>'
 html += '</div></div>'
 
-html += '<a href="https://afellowu.neocities.org/artworks">Artworks</a>'
-html += '<a href="https://afellowu.neocities.org/ocmain">OCs</a>'
+html += '<a href="/artworks">Artworks</a>'
+html += '<a href="/ocmain/index">OCs</a>'
 
 document.getElementById("navbartop").innerHTML = html;
